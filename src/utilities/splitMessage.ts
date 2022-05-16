aa5d50cda625be36b1e12db37c4e3feb04894f20
Mitchdev <admin@mitchdev.net>
2022-05-16 06:05:16 +0000

    Removed broken splitMessage

--- src/utilities/splitMessage.ts
+++ src/utilities/splitMessage.ts
@@ -9,15 +9,15 @@ import { Message, Util } from 'discord.js';
  * @example Util.splitMessage('console.log(\'Hello world\')', message, 'ts')
  */
 export default async function splitMessage(content: string | string[], message: Message, codeblock: string = null): Promise<void> {
-  if (typeof content === 'string') {
-    if (content.length > 0) {
-      if (codeblock !== null) content = Util.cleanCodeBlockContent(content);
-      content = Util.splitMessage(content);
-    } else return;
-  }
-  if (content.length > 0) {
-    const reply = await message.reply(`${codeblock ? `\`\`\`${codeblock}\n` : ''}${content[0]}${codeblock ? '```' : ''}`);
-    content.shift();
-    return await splitMessage(content, reply, codeblock);
-  } else return;
+  // if (typeof content === 'string') {
+  //   if (content.length > 0) {
+  //     if (codeblock !== null) content = Util.cleanCodeBlockContent(content);
+  //     content = Util.splitMessage(content);
+  //   } else return;
+  // }
+  // if (content.length > 0) {
+  //   const reply = await message.reply(`${codeblock ? `\`\`\`${codeblock}\n` : ''}${content[0]}${codeblock ? '```' : ''}`);
+  //   content.shift();
+  //   return await splitMessage(content, reply, codeblock);
+  // } else return;
 }
