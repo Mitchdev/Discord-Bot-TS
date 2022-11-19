6ad27f07f5747ae03a8a99b983b78ade1a0df9bd
Mitchdev <admin@mitchdev.net>
2022-11-19 13:30:44 +0000

    Error handling f1 update.

--- src/scheduled/f1StandingsUpdate.ts
+++ src/scheduled/f1StandingsUpdate.ts
@@ -25,47 +25,49 @@ export default new Scheduled('f1StandingsUpdate', 1800, true, async () => {
     const driverStanding: F1DriverStanding[] = await (await fetch(process.env.F1_DRIVERS_API)).json() as unknown as F1DriverStanding[];
     const constructorStanding: F1ConstructorStanding[] = await (await fetch(process.env.F1_CONSTRUCTORS_API)).json() as unknown as F1ConstructorStanding[];
 
-    const driverEmbed = new EmbedBuilder()
-      .setTitle(`Driver Standings after ${lastRound.track.name}`)
-      .setColor(Color.F1_RED)
-      .addFields([{
-        name: 'Position',
-        value: driverStanding.map((standing) => `**${standing.position}**`).join('\n'),
-        inline: true
-      }, {
-        name: 'Driver',
-        value: driverStanding.map((standing) => `${standing.driver.name}`).join('\n'),
-        inline: true
-      }, {
-        name: 'Points',
-        value: driverStanding.map((standing) => `${standing.points}`).join('\n'),
-        inline: true
-      }])
-      .setFooter({text: `ROUND ${nextRound.type === 'Testing' ? 0 : lastRound.round} / ${rounds.filter((round) => round.type === 'Round').length}`});
+    if (driverStanding.length > 0) {
+      const driverEmbed = new EmbedBuilder()
+        .setTitle(`Driver Standings after ${lastRound.track.name}`)
+        .setColor(Color.F1_RED)
+        .addFields([{
+          name: 'Position',
+          value: driverStanding.map((standing) => `**${standing.position}**`).join('\n'),
+          inline: true
+        }, {
+          name: 'Driver',
+          value: driverStanding.map((standing) => `${standing.driver.name}`).join('\n'),
+          inline: true
+        }, {
+          name: 'Points',
+          value: driverStanding.map((standing) => `${standing.points}`).join('\n'),
+          inline: true
+        }])
+        .setFooter({text: `ROUND ${nextRound.type === 'Testing' ? 0 : lastRound.round} / ${rounds.filter((round) => round.type === 'Round').length}`});
+        const driverMessage = await (client.channels.resolve(process.env.CHANNEL_F1) as TextChannel).messages.fetch(process.env.MESSAGE_F1_DRIVERS);
+        if (driverMessage.embeds[0].fields[2].value !== driverEmbed.data.fields[2].value) await driverMessage.edit({embeds: [driverEmbed]});
+    }
 
-    const constructorEmbed = new EmbedBuilder()
-      .setTitle(`Constructors Standings after ${lastRound.track.name}`)
-      .setColor(Color.F1_RED)
-      .addFields([{
-        name: 'Position',
-        value: constructorStanding.map((standing) => `**${standing.position}**`).join('\n'),
-        inline: true
-      }, {
-        name: 'Constructor',
-        value: constructorStanding.map((standing) => `${standing.constructor}`).join('\n'),
-        inline: true
-      }, {
-        name: 'Points',
-        value: constructorStanding.map((standing) => `${standing.points}`).join('\n'),
-        inline: true
-      }])
-      .setFooter({text: `ROUND ${nextRound.type === 'Testing' ? 0 : lastRound.round} / ${rounds.filter((round) => round.type === 'Round').length}`});
-
-    const driverMessage = await (client.channels.resolve(process.env.CHANNEL_F1) as TextChannel).messages.fetch(process.env.MESSAGE_F1_DRIVERS);
-    const constructorMessage = await (client.channels.resolve(process.env.CHANNEL_F1) as TextChannel).messages.fetch(process.env.MESSAGE_F1_CONSTRUCTORS);
-
-    if (driverMessage.embeds[0].fields[2].value !== driverEmbed.data.fields[2].value) await driverMessage.edit({embeds: [driverEmbed]});
-    if (constructorMessage.embeds[0].fields[2].value !== constructorEmbed.data.fields[2].value) await constructorMessage.edit({embeds: [constructorEmbed]});
+    if (constructorStanding.length > 0) {
+      const constructorEmbed = new EmbedBuilder()
+        .setTitle(`Constructors Standings after ${lastRound.track.name}`)
+        .setColor(Color.F1_RED)
+        .addFields([{
+          name: 'Position',
+          value: constructorStanding.map((standing) => `**${standing.position}**`).join('\n'),
+          inline: true
+        }, {
+          name: 'Constructor',
+          value: constructorStanding.map((standing) => `${standing.constructor}`).join('\n'),
+          inline: true
+        }, {
+          name: 'Points',
+          value: constructorStanding.map((standing) => `${standing.points}`).join('\n'),
+          inline: true
+        }])
+        .setFooter({text: `ROUND ${nextRound.type === 'Testing' ? 0 : lastRound.round} / ${rounds.filter((round) => round.type === 'Round').length}`});
+        const constructorMessage = await (client.channels.resolve(process.env.CHANNEL_F1) as TextChannel).messages.fetch(process.env.MESSAGE_F1_CONSTRUCTORS);
+        if (constructorMessage.embeds[0].fields[2].value !== constructorEmbed.data.fields[2].value) await constructorMessage.edit({embeds: [constructorEmbed]});
+    }
   } catch(error) {
     console.log(error);
   }
